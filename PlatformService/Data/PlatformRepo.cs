e7359ab19e088a3360ac02dd0da5bd6a05b55b9b
Lyès MAKHLOUFI <lyes.makhloufi@acelys.fr>
2022-05-27 21:01:35 +0200

    Creating platform repository

---
+++ PlatformService/Data/PlatformRepo.cs
@@ -0,0 +1,39 @@
+﻿using PlatformService.Models;
+
+namespace PlatformService.Data
+{
+    public class PlatformRepo : IPlatformRepo
+    {
+        private readonly AppDbContext _context;
+
+        public PlatformRepo(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public void CreatePlatform(Platform platform)
+        {
+            if(platform == null)
+            {
+                throw new ArgumentNullException(nameof(platform));
+            }
+            _context.Platforms.Add(platform);
+            SaveChanges();
+        }
+
+        public IEnumerable<Platform> GetAllPlatforms()
+        {
+            return _context.Platforms.ToList();
+        }
+
+        public Platform? GetPlatformById(int id)
+        {
+            return _context.Platforms.FirstOrDefault(e => e.Id == id);
+        }
+
+        public bool SaveChanges()
+        {
+            return (_context.SaveChanges() > 0 ? true : false); 
+        }
+    }
+}
